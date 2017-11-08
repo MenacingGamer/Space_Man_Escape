125e2ddb1818b74d9b7da2a0177845453f115026
DESKTOP-5Q2KHUR\Dennis <dennismenace82484@gmail.com>
2017-11-08 00:47:02 -0800

    set up camera control script
    camera now will follow the rocket

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public Rocket rocket;
+
+    private Vector3 offset;
+    // Use this for initialization
+    void Start () {
+        offset = transform.position - rocket.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = rocket.transform.position + offset;
+	}
+}
